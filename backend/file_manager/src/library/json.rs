795d3b28c26c63c52ba369465739d3cb07a05e06
Md Sadman <md.sadman.shams@gmail.com>
2025-05-01 12:55:08 +0900

    json builder implementation is completed

--- backend/file_manager/src/library/json.rs
+++ backend/file_manager/src/library/json.rs
@@ -1,11 +1,10 @@
-use std::{collections::HashMap, fmt::Display, vec};
+use std::{collections::HashMap, fmt::Display};
 
-use crate::{hashmap, logger::app_logger::Logger};
+use crate::logger::app_logger::Logger;
 
-pub struct Json {
-  json_map: Option<HashMap<String, String>>,
-}
+pub struct Json;
 
+#[allow(dead_code)]
 pub trait JsonParser {
   fn perser() -> impl JsonParser;
   fn parse(&self, json: String);
@@ -13,7 +12,7 @@ pub trait JsonParser {
 
 impl JsonParser for Json {
   fn perser() -> impl JsonParser {
-    Self { json_map: Option::None }
+    Self { }
   }
 
   fn parse(&self, json: String) {
@@ -21,38 +20,34 @@ impl JsonParser for Json {
   }
 }
 
+#[allow(dead_code)]
 pub trait JsonBuilder {
-  fn builder() -> impl JsonBuilder;
-  fn put(&mut self, key: impl Into<String>, value: impl Into<JsonValue>);
-  fn build(&self) -> String;
+  fn object() -> JsonObject;
+  fn array() -> JsonArray;
+  fn build(json_container: impl Into<JsonContainer>) -> String {
+    (json_container.into() as JsonContainer).result
+  }
 }
 
 impl JsonBuilder for Json {
-  fn builder() -> impl JsonBuilder {
-    Self { json_map: Option::Some(HashMap::new()) }
+  fn object() -> JsonObject {
+    JsonObject::new()
   }
 
-  fn put(&mut self, key: impl Into<String>, value: impl Into<JsonValue>) {
-    let json_key = key.into();
-    let json_value: JsonValue = value.into();
-    Logger::debug(format!("New Value | {} => {}", json_key, json_value));
-
-    if !json_value.dt.is_primitive() {
-      return;
-    }
-
-
-    self.json_map.as_mut().unwrap().insert(format!(r#""{}""#, json_key), json_value.value);
+  fn array() -> JsonArray {
+    JsonArray::new()
   }
+}
 
-  fn build(&self) -> String {
-    let map = self.json_map.as_ref().expect("Failed to achieve json map!");
-    let mut result = Vec::new();
-    for e in map {
-      result.push(format!("{}:{}", e.0, e.1));
-    }
+#[derive(Debug)]
+pub struct JsonValue {
+  dt: JsonType,
+  value: String,
+}
 
-    format!("{{ {} }}", result.join(","))
+impl Display for JsonValue {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    f.write_str(format!("JsonValue {{ dt: {}, value: {} }}", self.dt, self.value).as_str())
   }
 }
 
@@ -73,28 +68,82 @@ impl JsonType {
   }
 }
 
-#[derive(Debug)]
-pub struct JsonValue {
-  dt: JsonType,
-  value: String,
-}
+pub struct JsonNull;
 
-impl Display for JsonValue {
-  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-    f.write_str(format!("JsonValue {{ dt: {}, value: {} }}", self.dt, self.value).as_str())
+impl JsonNull {
+  pub fn new() -> Self { 
+    Self {} 
   }
 }
 
-struct JsonTypeNull;
-struct JsonTypeObject {
+pub struct JsonContainer {
+  result: String,
+}
+
+impl Into<JsonContainer> for JsonObject {
+  fn into(self) -> JsonContainer {
+    let mut result = Vec::new(); 
+    for e in self.object.iter() {
+      result.push(format!(r#""{}":{}"#, *e.0, *e.1));
+    }
+
+    JsonContainer { result: format!("{{{}}}", result.join(",")) }
+  }
+}
+
+impl Into<JsonContainer> for JsonArray {
+  fn into(self) -> JsonContainer {
+    let json_array: JsonValue = self.into();
+    JsonContainer { result: json_array.value }
+  }
+}
+
+pub struct JsonObject {
   object: HashMap<String, String>,
 }
-struct JsonTypeArray {
-  array: Vec<JsonTypeObject>,
+
+impl JsonObject {
+  pub fn new() -> Self {
+    Self { object: HashMap::new() }
+  }
+
+pub fn insert(&mut self, k: impl Into<String>, v: impl Into<JsonValue>) -> &mut Self {
+    let (json_key , json_value) = (k.into(), v.into());
+    Logger::debug(format!("Append Value | {} => {}", json_key, json_value));
+    self.object.insert(json_key, json_value.value);
+    self
+  }
+
+  pub fn remove(&mut self, k: impl Into<String>) -> &mut Self {
+    let json_key = k.into();
+    let json_value = self.object.remove(&json_key);
+    Logger::debug(format!("Delete Value | {} => {}", json_key, json_value.unwrap_or(String::new())));
+    self
+  }
+}
+
+pub struct JsonArray {
+  array: Vec<JsonObject>,
+}
+
+impl JsonArray {
+  fn new() -> Self {
+    Self { array: Vec::new() }
+  }
+
+  fn append(&mut self, o: JsonObject) -> &mut Self {
+    self.array.push(o);
+    self
+  }
+
+  fn remove(&mut self, index: usize) -> &mut Self {
+    let _ = self.array.remove(index);
+    self
+  }
 }
 
 // ARRAY
-impl Into<JsonValue> for JsonTypeArray {
+impl Into<JsonValue> for JsonArray {
   fn into(self) -> JsonValue {
     let mut v = Vec::new();
     for e in self.array {
@@ -107,20 +156,20 @@ impl Into<JsonValue> for JsonTypeArray {
 }
 
 // OBJECT 
-impl Into<JsonValue> for JsonTypeObject {
+impl Into<JsonValue> for JsonObject {
   fn into(self) -> JsonValue {
-    let mut v = Vec::new();
-    for e in self.object {
-      let json_value: JsonValue = e.1.into();
-      v.push(format!("\"{}\": {}", e.0, json_value.value)); 
+    let mut s = String::new();
+    for (i, (k, v)) in self.object.iter().enumerate() {
+      s.push_str(format!("\"{}\":{}", k, v).as_str());
+      if i < self.object.len() - 1 { s.push(',') };
     } 
 
-    JsonValue { dt: JsonType::Object, value: format!("{{ {} }}", v.join(",")) }
+    JsonValue { dt: JsonType::Object, value: format!("{{{}}}", s) }
   }
 }
 
 // NULL
-impl Into<JsonValue> for JsonTypeNull {
+impl Into<JsonValue> for JsonNull {
   fn into(self) -> JsonValue {
     JsonValue { dt: JsonType::Null, value: format!("null") }
   }
@@ -223,16 +272,67 @@ impl Into<JsonValue> for u8 {
 
 #[cfg(test)]
 mod tests {
-  use super::{Json, JsonBuilder};
+  use crate::library::json::JsonNull;
+
+use super::{Json, JsonBuilder};
 
   #[test]
-  fn json_value_test() {
-    let mut json_builder = Json::builder();
-    json_builder.put("key1", "string1");
-    json_builder.put("key2", 123);
-    json_builder.put("key3", true);
+  fn json_array_test() {
+    let mut json_object_2 = Json::object();
+    json_object_2.insert("key1", "string2");
+    json_object_2.insert("key2", 456);
+    json_object_2.insert("key3", false);
+    json_object_2.insert("key4", JsonNull::new());
 
-    let json = json_builder.build();
+    let mut json_object_3 = Json::object();
+    json_object_3.insert("key1", "string2");
+    json_object_3.insert("key2", 456);
+    json_object_3.insert("key3", false);
+    json_object_3.insert("key4", JsonNull::new());
+
+    let mut json_array = Json::array();
+    json_array.append(json_object_2).append(json_object_3);
+
+    let json = Json::build(json_array);
+    println!("--- Generated Json ---");
+    println!("{}", json);
+    println!("--- Generated Json ---");
+  }
+
+  #[test]
+  fn json_object_test() {
+    let mut json_object_1 = Json::object();
+    json_object_1.insert("key1", "string1");
+    json_object_1.insert("key2", 456);
+    json_object_1.insert("key3", false);
+    json_object_1.insert("key4", JsonNull::new());
+
+    let mut json_object_2 = Json::object();
+    json_object_2.insert("key1", "string2");
+    json_object_2.insert("key2", 456);
+    json_object_2.insert("key3", false);
+    json_object_2.insert("key4", JsonNull::new());
+
+    let mut json_object_3 = Json::object();
+    json_object_3.insert("key1", "string2");
+    json_object_3.insert("key2", 456);
+    json_object_3.insert("key3", false);
+    json_object_3.insert("key4", JsonNull::new());
+
+    let mut json_array = Json::array();
+    json_array.append(json_object_2);
+    json_array.append(json_object_3);
+
+    let mut json_object = Json::object();
+    json_object
+      .insert("key1", "string0")
+      .insert("key2", 123)
+      .insert("key3", true)
+      .insert("key4", JsonNull::new())
+      .insert("object_1", json_object_1)
+      .insert("array", json_array);
+
+    let json = Json::build(json_object);
     println!("--- Generated Json ---");
     println!("{}", json);
     println!("--- Generated Json ---");
--- backend/file_manager/src/router/get_routes.rs
+++ backend/file_manager/src/router/get_routes.rs
@@ -1,16 +1,18 @@
-use crate::{library::json::{Json, JsonBuilder}, parser::http_response::HttpResponse};
+use crate::{library::json::{Json, JsonBuilder, JsonNull}, parser::http_response::HttpResponse};
 
 pub struct Get;
 
 impl Get {
   pub fn home() -> String {
-    let mut json_builder = Json::builder();
-    json_builder.put("path", "home");
-    json_builder.put("method", "get");
-    json_builder.put("number", 123);
-    json_builder.put("is_alright", true);
+    let mut json_object = Json::object();
+    json_object.insert("path", "home");
+    json_object.insert("method", "get");
+    json_object.insert("number", 123);
+    json_object.insert("is_alright", true);
+    json_object.insert("is_null", JsonNull::new());
 
-    let contents = json_builder.build();
+
+    let contents = Json::build(json_object);
     let http_response = HttpResponse::new("200", "Ok", contents);
     http_response.construct()
   } 
